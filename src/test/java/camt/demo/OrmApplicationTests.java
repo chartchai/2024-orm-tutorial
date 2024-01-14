393865ff110903675a0f8c9bc5b2c1a483428a16
dto <chartchai.d@cmu.ac.th>
2024-01-14 00:29:21 +0000

    initial commit

---
+++ src/test/java/camt/demo/OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package camt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
